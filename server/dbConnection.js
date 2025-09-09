e0596c61b5018daabe9f40341c5b6c53d90685f9
gabby <hannahfrench1709@hotmail.com>
2025-09-09 11:33:46 +0100

    feat: set up supabase, dbConnection.js and server.js

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,3 +1,11 @@
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const dbConnectionString = process.env.DATABASE_URL;
+
+//setting up the pool
+export const db = new pg.Pool({
+    connectionString: dbConnectionString,
+});
